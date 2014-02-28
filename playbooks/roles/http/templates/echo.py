d8cc4430ae4f8dbc6a33ddc7f1d036d8f06df359
Dolph Mathews <dolph.mathews@gmail.com>
2014-02-27 21:23:52 -0600

    add minimal auth_token config

--- playbooks/roles/http/templates/echo.py
+++ playbooks/roles/http/templates/echo.py
@@ -10,7 +10,22 @@ from keystoneclient.middleware import auth_token
 
 AUTH_TOKEN_CONF = {
     'auth_protocol': 'http',
-    'admin_token': 'ADMIN'}
+    'auth_host': '192.168.111.222',
+    'auth_port': 35357,
+    'auth_uri': 'http://192.168.111.222:35357/',
+    'admin_token': 'ADMIN',
+    'admin_user': None,
+    'admin_password': None,
+    'admin_tenant_name': 'admin',
+    'certfile': None,
+    'keyfile': None,
+    'cafile': None,
+    'memcached_servers': ['127.0.0.1:11211'],
+    'token_cache_time': 300,
+    'revocation_cache_time': 60,
+    'memcache_security_strategy': 'ENCRYPT',
+    'memcache_secret_key': '5bdc5pilx0o7re34z3yc2yt4x',
+    'include_service_catalog': True}
 
 
 def echo_app(environ, start_response):
