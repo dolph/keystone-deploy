b2ae944c908ad7b43514283db3211dc0d5106bef
Dolph Mathews <dolph.mathews@gmail.com>
2015-03-18 18:13:49 +0000

    bootstrap deployment using setUpClass

--- test_exercises.py
+++ test_exercises.py
@@ -13,52 +13,103 @@ KEYSTONE_ENDPOINT = os.environ.get(
     'KEYSTONE_ENDPOINT', 'http://%s:35357/' % HOST)
 
 
+def unique():
+    return uuid.uuid4().hex
+
+
 class Base(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         c = v3_client.Client(
             token='ADMIN',
             endpoint=KEYSTONE_ENDPOINT + 'v3')
 
+        domain = c.domains.get('default')
+
+        roles = c.roles.list(name='admin')
+        if roles:
+            role = roles[0]
+        else:
+            role = c.roles.create(name='admin')
+
+        groups = c.groups.list(domain=domain, name='admin')
+        if groups:
+            group = groups[0]
+        else:
+            group = c.groups.create(domain=domain, name='admin')
+
+        c.roles.grant(group=group, domain=domain, role=role)
+
+        projects = c.projects.list(domain=domain, name='admin')
+        if projects:
+            project = projects[0]
+        else:
+            project = c.projects.create(domain=domain, name='admin')
+
+        c.roles.grant(group=group, project=project, role=role)
+
+        password = 'password'
+        users = c.users.list(domain=domain, name='admin')
+        if users:
+            user = users[0]
+        else:
+            user = c.users.create(
+                domain=domain, name='admin', password=password)
+
+        c.users.add_to_group(user=user, group=group)
+
+        services = c.services.list(
+            name='Keystone', type='identity')
+        if services:
+            service = services[0]
+        else:
+            service = c.services.create(
+                name='Keystone', type='identity')
+
+        endpoints = c.endpoints.list()
+        if not [x for x in endpoints if x.interface == 'public']:
+            c.endpoints.create(
+                service=service,
+                interface='public',
+                url=KEYSTONE_ENDPOINT + 'v3')
+        if not [x for x in endpoints if x.interface == 'admin']:
+            c.endpoints.create(
+                service=service,
+                interface='admin',
+                url=KEYSTONE_ENDPOINT + 'v3')
+
+    def setUp(self):
+        c = v3_client.Client(
+            username='admin',
+            password='password',
+            project_name='admin',
+            auth_url=KEYSTONE_ENDPOINT + 'v3')
+
         self.domain = c.domains.get('default')
 
-        role = c.roles.create(name='admin')
+        role = c.roles.create(name=unique())
         self.addCleanup(c.roles.delete, role)
 
-        group = c.groups.create(domain=self.domain, name='admin')
+        group = c.groups.create(domain=self.domain, name=unique())
         self.addCleanup(c.groups.delete, group)
 
         c.roles.grant(group=group, domain=self.domain, role=role)
         self.addCleanup(
             c.roles.revoke, group=group, domain=self.domain, role=role)
 
-        self.project = c.projects.create(domain=self.domain, name='admin')
+        self.project = c.projects.create(domain=self.domain, name=unique())
         self.addCleanup(c.projects.delete, self.project)
 
         c.roles.grant(group=group, project=self.project, role=role)
         self.addCleanup(
             c.roles.revoke, group=group, project=self.project, role=role)
 
-        self.password = 'password'
+        self.password = unique()
         self.user = c.users.create(
-            domain=self.domain, name='admin', password=self.password)
+            domain=self.domain, name=unique(), password=self.password)
         self.addCleanup(c.users.delete, self.user)
 
         c.users.add_to_group(user=self.user, group=group)
-
-        service = c.services.create(
-            name='Keystone', type='identity')
-        self.addCleanup(c.services.delete, service)
-
-        public_endpoint = c.endpoints.create(
-            service=service,
-            interface='public',
-            url=KEYSTONE_ENDPOINT + 'v3')
-        self.addCleanup(c.endpoints.delete, public_endpoint)
-        admin_endpoint = c.endpoints.create(
-            service=service,
-            interface='admin',
-            url=KEYSTONE_ENDPOINT + 'v3')
-        self.addCleanup(c.endpoints.delete, admin_endpoint)
 
     def assertIdentityContext(self, context):
         self.assertEqual('Confirmed', context['HTTP_X_IDENTITY_STATUS'])
@@ -107,7 +158,7 @@ class Base(unittest.TestCase):
 
 class Common(object):
     def test_unauthorized_request(self):
-        self.exercise_token(uuid.uuid4().hex, expected_status=401)
+        self.exercise_token(unique(), expected_status=401)
 
     def test_unscoped_to_project_scoped(self):
         unscoped = self.client.Client(**{
@@ -178,23 +229,26 @@ class V3(Base, Common):
         r = self.exercise_token(token)
         self.assertDomainScopedContext(self.domain, r.json())
 
-    def test_domain_list(self):
+    def test_accessible_projects_list(self):
         project_scoped = self.client.Client(
             user_id=self.user.id,
             password=self.password,
             project_id=self.project.id,
             auth_url=KEYSTONE_ENDPOINT + self.version)
-        self.assertTrue(project_scoped.auth_token)
+        self.assertProjectScopedToken(project_scoped.auth_token)
 
-        domains = project_scoped.domains.list()
+        projects = project_scoped.projects.list(user=self.user)
 
-        self.assertEqual(1, len(domains))
+        self.assertEqual(1, len(projects))
 
-        domain = domains[0]
-        self.assertEqual('default', domain.id)
-        self.assertEqual('Default', domain.name)
-        self.assertEqual(True, domain.enabled)
-        self.assertTrue(domain.links['self'].endswith('/v3/domains/default'))
+        project = projects[0]
+        self.assertEqual(self.project.id, project.id)
+        self.assertEqual(self.project.name, project.name)
+        self.assertEqual(self.project.enabled, project.enabled)
+        self.assertEqual(self.project.description, project.description)
+        self.assertTrue(
+            project.links['self'].endswith(
+                '/v3/projects/%s' % self.project.id))
 
     def test_unscoped_to_domain_scoped(self):
         unscoped = self.client.Client(
