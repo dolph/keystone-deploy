bded51791183f6177462eac92803ceadffcdadec
Dolph Mathews <dolph.mathews@gmail.com>
2015-04-30 16:59:00 +0000

    split echo and keystone into separate roles

--- playbooks/roles/echo/templates/echo.py
+++ playbooks/roles/echo/templates/echo.py
@@ -15,8 +15,8 @@ logging.basicConfig()
 
 AUTH_TOKEN_CONF = {
     'auth_plugin': 'password',
-    'auth_uri': 'http://{{ ansible_default_ipv4["address"] }}:35357/',
-    'auth_url': 'http://{{ ansible_default_ipv4["address"] }}:35357/',
+    'auth_uri': 'http://{{ keystone_host }}:35357/',
+    'auth_url': 'http://{{ keystone_host }}:35357/',
     'username': 'admin',
     'user_domain_id': 'default',
     'password': 'password',
